1e0f41ad117fcff74ba68c916257db25cc89c2ff
ruban <e.ruban@cft.ru>
2022-11-15 03:06:07 +0700

    MVP-7 Добавил иконки для статусов загрузки скриптов

--- component-res/src/main/kotlin/dev/itssho/module/component/resources/ResourcesUtils.kt
+++ component-res/src/main/kotlin/dev/itssho/module/component/resources/ResourcesUtils.kt
@@ -6,7 +6,15 @@ import java.net.URL
 import javax.imageio.ImageIO
 
 
-fun <T: Any> T.getResUrl(resPath: String): URL = this.javaClass.getResource(resPath)
+fun <T: Any> T.getResUrl(resPath: String): URL {
+	if (!resPath.startsWith("/")) {
+		throw IllegalArgumentException("Resource path must start with '/'")
+	}
+
+	val clazz = this.javaClass
+	val resUrl = clazz.getResource(resPath)
+	return resUrl
+}
 
 fun loadIcon(url: URL): Image = ImageIO.read(url)
 
